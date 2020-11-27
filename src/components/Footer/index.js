2b87ba6a1428ee51397fde01a7c3fa41d188d2e6
Andy Montalvo <59586600+amontalvof@users.noreply.github.com>
2020-11-27 12:48:50 -0600

    added linkedin icon

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { FooterWrapper } from "./FooterStyle";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <FooterWrapper>
       <div className="flex-container">
         <div>
+          <a className="iconosSociales" href={"https://www.linkedin.com/in/andy-montalvo-fern%C3%A1ndez-6a9a37148/"}>
+            <FaLinkedin />
+          </a>{" "}
           <a className="iconosSociales" href={"https://github.com/amontalvof/react-antique-tech-store/"}>
             <FaGithub />
           </a>
